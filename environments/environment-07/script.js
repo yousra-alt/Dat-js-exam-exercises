1cf4cf4bc1bfacada3576c51cef5ae7cfc66d34b
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-05-30 12:36:07 +0200

    øvelse i undervnsning

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+const students = [];
+
+function start() {
+  document.querySelector("#create-student-form").addEventListener("submit", showStudent);
+}
+
+function formClicked(event) {
+  event.preventDefault();
+  const form = event.target;
+
+  students.push({ name: form.name.value, email: form.email.value, age: Number(form.age.value) });
+  showStudent(students);
+}
+
+//   const name = form. name.value;
+//   const email = form.email.value;
+//   const age = form.age.value;
+
+//     CreateStudent(name, email, age);
+
+//   showStudent()
+
+function CreateStudent(name, age, email) {
+  const student = {
+    name: name,
+    age: Number(age),
+    email: email,
+  };
+  console.log(student);
+}
+
+function showStudent() {
+  document.querySelector("#students-table-body").innerHTML = "";
+  students.sort((a, b) => a.name.localeCompare(b.name));
+  // lav dom
+}
