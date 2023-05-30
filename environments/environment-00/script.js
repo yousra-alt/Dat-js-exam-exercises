c37b91be168b09e1fd1ecee454116f4fb1e7b8ae
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-05-30 11:04:10 +0200

    opgave1

--- environments/environment-00/script.js
+++ environments/environment-00/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+    document.querySelector("#btn-knap").addEventListener("click", buttonClicked);
+    hideTextClickec()
+}
+
+function buttonClicked () {
+console.log("Det virker");
+}
+
+function showText () {
+document.querySelector("#result_failure").classList.add("show");
+
+}
+
+function hideTextClickec () {
+document.querySelector("#result_failure").classList.add("hide");
+document.querySelector("#result_succes").classList.add("hide");
+
+
+}
