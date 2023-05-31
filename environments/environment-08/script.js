18e592f6e65f7aee3ef12a38958e5cc941588088
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-05-31 11:14:17 +0200

    øvelse.

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+
+let songs = [];
+function start() {
+
+}
+
+
+function createSong(artist, title, duation) {
+    const newSong = {
+        artist: artist,
+        title: title,
+        duration: duation,
+    }
+    console.log(newSong)
+    // *laver objekt
+}
+
+
+function showSongs(songs) {
+document.querySelector("#song-list").innerHTML="";
+
+
+}
