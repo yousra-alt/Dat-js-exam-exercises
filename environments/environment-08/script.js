32c6b929bdaa01413378647821d6a2b7e48cbdc5
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-06-01 13:17:23 +0200

    øvelse

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,45 @@
 "use strict";
+
+
+
+window.addEventListener("load", start);
+
+let songs = [];
+
+async function start() {
+    songs = await getSongs();
+
+    console.log(songs)
+    showSongs()
+}
+
+
+async function getSongs() {
+    const response = await fetch("playlist.json");
+    const playlist = response.json();
+    return playlist;
+}
+
+
+function showSongs() {
+ for (const song of songs) {
+
+ const html = /*html*/ `
+<li>${song.title}</li> 
+<button>Remove</button>
+`;
+
+document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+// document.querySelector("#songlist li:last-child button").addEventListener("click", () =>removeSong(song));
+
+}
+}
+
+function removeSong () {
+
+console.log(songToRemove);
+songs = songs.filter(song => song.title!= songToRemove.title)
+}
+
+
+
