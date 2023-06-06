e16764717bdf1330f710e8b787ee6890acc3ce78
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-06-06 15:39:00 +0200

    øvelse 15

--- environments/environment-06/script.js
+++ environments/environment-06/script.js
@@ -1,1 +1,54 @@
 "use strict";
+// ØVELSE 15
+window.addEventListener("load", initApp);
+
+let products = [];
+let basket = [];
+
+async function initApp() {
+  products = await getProducts();
+  console.log(products);
+  showProducts(products);
+}
+
+async function getProducts() {
+  const response = await fetch("products.json");
+  const data = await response.json();
+  return data;
+}
+
+function showProducts() {
+  for (const product of products) {
+    document.querySelector("#products").insertAdjacentHTML(
+      "beforeend",
+      /*HTML*/ `
+
+  <article>
+<h3>${product.name}</h3>
+<p>vægt${product.weight} g</p>
+<p>pris${product.price},-</p>
+<button>Læg i kurv</button>
+</article>
+
+
+`
+    );
+    document.querySelector("#products article:last-child button").addEventListener("click", () => addToBasket(product));
+  }
+}
+
+function addToBasket(product) {
+  basket.push(product);
+
+  document.querySelector("tbody").insertAdjacentHTML(
+    "beforeend",
+    /*HTML*/ `
+
+            <tr>
+              <td>${product.name}PRODUKTNAVN</td>
+            //   <td>PRIS PR STK${product.price},-</td>
+            </tr>
+
+`
+  );
+}
--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,49 @@
 "use strict";
+
+let songs = [
+  { artist: "Taylor Swift", title: "Blank space", duration: "4:33", counter: 0 },
+  { artist: "Beastie Boys", title: "Sabotage", duration: "3:02", counter: 0 },
+  { artist: "Skrillex", title: "Bangarang", duration: "3:35", counter: 0 },
+  { artist: "Wolfgang Amadeus Mozart", title: "Eine kleine Nachtmusik", duration: "5:45", counter: 0 },
+  { artist: "Coldplay", title: "Yellow", duration: "4:27", counter: 0 },
+  { artist: "Metallica", title: "Enter Sandman", duration: "5:32", counter: 0 },
+];
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+  showSongs(songs);
+}
+
+function showSongs(songs) {
+  for (const song of songs) {
+    document.querySelector("#songlist").insertAdjacentHTML(
+      "beforeend",
+      /*HTML*/ `
+<li>${song.artist}</li>
+<li>${song.title}</li>
+<li>${song.duration}</li>
+<button>upvote</button>
+`
+    );
+
+    document.querySelector("#songlist button:last-child").addEventListener("click", () => upvoteSong(song));
+  }
+}
+
+function upvoteSong(song) {
+  song.counter++;
+  console.log(song.counter);
+
+  document.querySelector("#songlist").innerHTML = "";
+  
+}
+// # Øvelse 23
+
+// Environment: `environment-08`
+
+// *Prøveeksamen #5 (sæt 2)*
+
+// 1. Opret en liste af sang-objekter, hvor hver sang indeholder `artist`, `title` og `duration` (i form af minutter:sekunder, fx 3:21)
+// 2. Lav en funktion til at udskrive listen af sange på websiden - tilføj en `upvote`-knap til hver sang.
+// 3. Få `upvote`-knappen til flytte den pågældende sang én tak op i listen - udskriv derefter listen igen.
