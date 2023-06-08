5e35be708d610d938a2d0be64c727e4ade169fcf
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-06-08 16:56:07 +0200

    filter og sort i 6

--- environments/environment-06/script.js
+++ environments/environment-06/script.js
@@ -1,1 +1,53 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+let users = [
+  { name: "Drake", role: "corny nigga", age: "34" },
+  { name: "Yousra", role: "BossMan", age: "2" },
+  { name: "Benedikte", role: "bitch", age: "28" },
+  { name: "Adam", role: "Bitchens bitch", age: "23" },
+  { name: "Charlie", role: "Bossman", age: "42" },
+  { name: "Elefant", role: "bossMan", age: "12" },
+];
+
+function initApp() {
+
+    // Sortering
+  users.sort(sortByName);
+  console.log(users);
+
+  users.sort(sortReverse);
+  console.log(users);
+
+  users.sort(sortByAge);
+  console.log(users);
+
+//   filter
+  const role = users.filter((user) => user.role.toLowerCase().includes("bossman"));
+  console.log(role);
+
+  const roles = users.filter((user) => user.role.toUpperCase().includes("BOSSMAN"));
+  console.log(roles);
+
+  const name = users.filter((user) => user.name.includes("Yousra"));
+  console.log(name);
+users.sort(sortByAgeAgain);
+console.log(users)
+}
+
+function sortByName(a, b) {
+  return a.name.localeCompare(b.name);
+}
+
+function sortReverse(a, b) {
+  return b.name.localeCompare(a.name);
+}
+
+function sortByAge(a, b) {
+  return b.age - a.age;
+}
+
+function sortByAgeAgain(a, b) {
+return Number(a.age) - Number(b.age);
+}
+
--- exercises/exercise-09.md
+++ exercises/exercise-09.md
@@ -5,3 +5,4 @@ Environment: `environment-04`
 1. Importer `teachers`-listen i `script.js`.
 2. Lav en funktion i `script.js`, der viser listen af alle `teacher`-objekter på websiden.
 3. Lav en funktion der kan sortere listen efter `name` og en anden funktion, der kan sortere listen efter `email`.
+.
